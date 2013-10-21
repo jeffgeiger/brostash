c46334f03bcdf4baaf3890916408824e81dac808
jeff.geiger <jeff@ngmocnd3011sx.local>
2013-10-20 23:43:01 -0500

    Almost there, but trailing newlines and tabs still an issue.

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'mandrill'
 require 'pp'
 #require 'mongo_mapper'
 
--- routes/main.rb
+++ routes/main.rb
@@ -3,4 +3,12 @@ class Status < Sinatra::Application
 	get "/" do
 		erb :index #, :locals => {:services => Service.all}
 	end
+
+	get "/about" do
+		erb :about
+	end
+
+	post "/produce" do
+		erb :produce, :locals => {:broFields => params['broFields']}
+	end
 end
