7caef135ea5b03d4329dc5ff24359be1cd7ef644
Jeff Geiger <jeff@jeffgeiger.com>
2013-10-29 00:28:14 -0500

    Ready for GitHub

--- app.rb
+++ app.rb
@@ -1,14 +1,9 @@
 require 'sinatra'
 require 'pp'
-#require 'mongo_mapper'
 
 class Status < Sinatra::Application
-	enable :sessions
-#  configure do
-#    MongoMapper.setup({'production' => {'uri' => ENV['MONGOLAB_URI']}}, 'production')
-#  end
+#	enable :sessions
 end
 
-require_relative 'helpers/init'
-#require_relative 'models/init'
 require_relative 'routes/init'
+
--- helpers/settings.rb
+++
@@ -1,7 +0,0 @@
-class Settings
-  def self.all
-    require 'yaml'
-    @@settings ||= YAML.load_file(File.join(File.dirname(__FILE__), "../config/settings.yml"))
-    @@settings
-  end
-end
